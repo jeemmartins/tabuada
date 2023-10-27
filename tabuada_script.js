df8edf6111e6b0e200b5a6820b62fe94690eb1fa
Jéssica Martins <44903365+jeemmartins@users.noreply.github.com>
2023-10-26 23:40:15 -0300

    Add files via upload

---
+++ tabuada_script.js
@@ -0,0 +1,21 @@
+function Tabuada() {
+  var num = document.getElementById('num')
+  var end = document.getElementById('end')
+
+  if (num.value.length == 0 || end.value.length == 0) {
+    window.alert('[Erro] - Confira os Dados!')
+  } else {
+    var n = Number(num.value)
+    var e = Number(end.value)
+    var x = 0
+    var r = ''
+    var res = window.document.getElementById('resposta')
+    res.innerHTML = `A tabuada do ${n} é: <br>`
+
+    do {
+      r = n * x
+      res.innerHTML += `${n} * ${x} = ${r} <br>`
+      x++
+    } while (x <= e)
+  }
+}
